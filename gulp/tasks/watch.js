408db36c6fd2ba66397beac24f70eaae79a18c4d
Brad <b.brad.wheeler@gmail.com>
2017-05-10 14:10:15 -0400

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+
+  });
+
+  watch('./app/index.html', function() {
+
+    browserSync.reload();
+
+  });
+//watches for any change to css tastk
+  watch('./app/assets/styles/**/*.css', function() {
+
+    gulp.start('cssInject');
+
+  });
+
+});
+
+
+//When css changes occur, injects css changes AFTER the dependency of the styles task pre-processes our css.
+gulp.task('cssInject', ['styles'], function(){
+
+  return gulp.src('./app/temp/styles/styles.css')
+      .pipe(browserSync.stream());
+});
